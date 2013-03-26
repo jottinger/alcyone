c2f243f4d37297890e52488ff95f09faf0575d93
Joseph B. Ottinger <joeo@enigmastation.com>
2013-03-26 17:09:47 -0400

    Committing project changes, plus inverting notes for the actual pedals

--- alcyone.cpp
+++ alcyone.cpp
@@ -149,9 +149,9 @@ void loop() {
         int state=debouncer[pin].debounce(decode(datum, pin%8));
         if(state!=previousState[pin]) {
             if(previousState[pin]) { // new state: OFF
-                noteOff(pin);
+                noteOff(13-pin); // when wired, pin 0 is the HIGH C
             } else {
-                noteOn(pin);
+                noteOn(13-pin);
             }
             previousState[pin]=state;
         }
