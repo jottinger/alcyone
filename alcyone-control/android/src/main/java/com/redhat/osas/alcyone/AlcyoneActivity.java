90c9ca302c82a7cc0fe1ce7ff2ef81a09d220951
Joseph B. Ottinger <joeo@enigmastation.com>
2013-04-02 08:25:32 -0400

    Starting Android Application

---
+++ alcyone-control/android/src/main/java/com/redhat/osas/alcyone/AlcyoneActivity.java
@@ -0,0 +1,15 @@
+package com.redhat.osas.alcyone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: jottinge
+ * Date: 4/2/13
+ * Time: 8:24 AM
+ */
+public class AlcyoneActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
