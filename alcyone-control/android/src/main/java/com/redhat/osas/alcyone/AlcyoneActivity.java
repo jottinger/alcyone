af006d1d1cf1d0eafcfe677488be18cece3c4990
Joseph B. Ottinger <joeo@enigmastation.com>
2013-04-02 11:52:35 -0400

    revised UI, balanced

--- alcyone-control/android/src/main/java/com/redhat/osas/alcyone/AlcyoneActivity.java
+++ alcyone-control/android/src/main/java/com/redhat/osas/alcyone/AlcyoneActivity.java
@@ -11,5 +11,6 @@ import android.os.Bundle;
 public class AlcyoneActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
     }
 }
