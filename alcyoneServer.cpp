de98e3cdba24e5df76da6d88c86444e3cd324e65
Joseph B. Ottinger <joeo@enigmastation.com>
2013-04-03 11:13:56 -0400

    Updating license to GPL

--- alcyoneServer.cpp
+++ alcyoneServer.cpp
@@ -7,6 +7,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+/*
+    Copyright 2012- by Joseph B. Ottinger.
+
+    This file is part of Alcyone.
+
+    Alcyone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Alcyone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Alcyone.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 int ALCYONE_SERVER_PORT=8090;
 
